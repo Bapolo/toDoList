0241650a9e35a296e917801e74693a5cd0432ab6
Jesus Pedro Bapolo <145621406+Bapolo@users.noreply.github.com>
2024-07-14 17:48:40 +0100

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -14,21 +14,34 @@ window.addEventListener("load", () => {
             li.innerHTML = `<input type="checkbox" class="itemCheckbox"> ${entradaDotexto.value} <button class="botaoApagar">${'<i class="fa-solid fa-trash"></i>'}</button> `
             listaDeTarefas.appendChild(li)
             entradaDotexto.value = ""
+
+            const checkbox = li.querySelector(".itemCheckbox")
+
+            checkbox.addEventListener("change", (event) =>
+            {
+                const paiDoCheckBox = event.target.closest('li')
+
+                if (event.target.checked)
+                {
+                    paiDoCheckBox.style.textDecoration = 'line-through'
+                }
+                else
+                {
+                    paiDoCheckBox.style.textDecoration = 'none'
+                }
+            })
+
+            const botaoApagar = li.querySelector(".botaoApagar")
+
+            botaoApagar.addEventListener("click", (event) =>
+            {
+                const paiDoBotao = event.target.closest('li')
+                paiDoBotao.parentNode.removeChild(paiDoBotao)
+
+            })
     
         })
     
-        if (listaDeTarefas.lenght)
-        {
-            const checkbox = li.querySelector(".itemCheckbox")
-    
-            checkbox.addEventListener("change", (event) =>
-            {
-                if (event.target.checked)
-                {
-                    console.log("Elemento preenchido")
-                }
-            }) 
-        }
   
     formulario.onsubmit = (event) => event.preventDefault()
 })
